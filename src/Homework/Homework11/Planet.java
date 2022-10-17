c9b6d7a51f50e0174205ae9615fd000ac9d844f3
Aisha <ivoninaanastasia16@gmail.com>
2022-10-17 16:19:02 +0300

    дз планеты 2.0

--- src/Homework/Homework11/Planet.java
+++ src/Homework/Homework11/Planet.java
@@ -28,23 +28,11 @@ public enum Planet {
         return radius;
     }
 
-    public void setRadius(int radius) {
-        this.radius = radius;
-    }
-
     public int getWeight() {
         return weight;
     }
 
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
